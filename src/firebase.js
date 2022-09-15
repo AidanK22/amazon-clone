ca5626b43b4cc97ad8548784e119f9f2ba0bb129
AidanK22 <aidankop1@gmail.com>
2022-09-15 13:47:52 -0500

    initial commit amazo-clone react app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCLHnhYhc1M96L9pViAPXcUCHBFDR7Cv2g",
+    authDomain: "clone-32eef.firebaseapp.com",
+    projectId: "clone-32eef",
+    storageBucket: "clone-32eef.appspot.com",
+    messagingSenderId: "670896709538",
+    appId: "1:670896709538:web:d02008ebe067f8d9dd1cfb",
+    measurementId: "G-TZX7XFJDJX"
+    };
